5bfc9d878da4cc153eb9bb90539311e22bef7123
Jen Chartier <54649817+JChartier001@users.noreply.github.com>
2020-01-30 23:48:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,7 @@
-const express = require("express");
-const server = express();
-const cors = require("cors");
-const helmet = require("helmet");
-require("dotenv").config();
-const port = process.env.PORT || 80;
-const patientRoute = require("./routes/patientRoute/patientRoute");
-const driverRoute = require("./routes/driverRoute/driverRoute");
+const server = require("./api/server.js");
 
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
+const PORT = process.env.PORT || 4000;
 
-server.use("/api/patient", patientRoute);
-server.use("/api/driver", driverRoute);
-
-server.use((err, req, res, next) => {
-  console.log("Error:", err);
-  res.status(500).json({
-    message: "Server Error"
-  });
-});
-
-server.get("/", (req, res) => {
-  res.status(200).json({ api: "up", dbenv: process.env.DB_ENV });
-});
-
-server.listen(port, () => {
-  console.log(`\n=> Server up at http://localhost:${port}\n`);
-});
-
-server.get("/", (req, res) => {
-  res.status(200).json({ api: "up", dbenv: process.env.DB_ENV });
+server.listen(PORT, () => {
+    console.log(`\n** Server running on port: ${PORT} **\n`);
 });
